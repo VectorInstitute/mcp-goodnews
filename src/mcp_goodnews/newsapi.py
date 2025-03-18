83c19f58401d34bc535ac0197c057041b7e15741
Val Andrei Fajardo <andrei@nerdai.io>
2025-03-18 18:00:52 -0400

    newsapi models

---
+++ src/mcp_goodnews/newsapi.py
@@ -0,0 +1,11 @@
+"""Pydantic BaseModels for NewsAPI.org Response Objects."""
+
+from pydantic import BaseModel
+
+
+class NewsAPIArticle(BaseModel):
+    ...
+
+
+class NewsAPIResponse(BaseModel):
+    ...
