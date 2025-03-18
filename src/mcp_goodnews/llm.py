01aa9c000b97f8844a22cc693810d8161f143ee5
Val Andrei Fajardo <andrei@nerdai.io>
2025-03-18 17:24:26 -0400

    use anthropic sdk

--- src/mcp_goodnews/llm.py
+++ src/mcp_goodnews/llm.py
@@ -1,1 +1,30 @@
 """This module contains logic for the llm to select good news."""
+
+import asyncio
+import os
+
+from anthropic import AsyncAnthropic
+
+client = AsyncAnthropic(
+    api_key=os.environ.get(
+        "ANTHROPIC_API_KEY"
+    ),  # This is the default and can be omitted
+)
+
+
+async def main() -> None:
+    message = await client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, Claude",
+            }
+        ],
+        model="claude-3-5-sonnet-latest",
+    )
+    print(message.content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
