0e237eeea9b87d86c8025268faafb4480028a4c8
Val Andrei Fajardo <andrei@nerdai.io>
2025-03-18 23:03:16 -0400

    unit test for article source

--- tests/test_newsapi_models/test_schema.py
+++ tests/test_newsapi_models/test_schema.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from mcp_goodnews.newsapi import NewsAPIResponse
+from mcp_goodnews.newsapi import ArticleSource, NewsAPIResponse
 
 
 def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
@@ -9,3 +9,11 @@ def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
     assert response.status == "ok"
     assert response.total_results == 10
     assert all(a.source.id_ == "bbcnews" for a in response.articles)
+
+
+def test_article_source_serialization() -> None:
+    source_dict = {"id": "fake_source_id", "name": "fake_name"}
+    source = ArticleSource.model_validate(source_dict)
+    serialized = source.model_dump(by_alias=True)
+
+    assert serialized == source_dict
