e0f8715362395c6a92044352a111b366545a7f81
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-03-19 01:43:44 -0400

    `GoodnewsRanker` and more unit tests (#1)
    * add assert for source
    
    * unit test for article source
    
    * add unit tests for more serialization of schema obs
    
    * goodnews ranker
    
    * add unit test for goodnews ranker init
    
    * unit test for get client
    
    * test _format_articles
    
    * unit test for prepare chat messages
    
    * don't use client in test
    
    * unit test for rank_articles
    
    * add rank instruction template

---
+++ src/mcp_goodnews/goodnews_ranker.py
@@ -0,0 +1,78 @@
+import json
+import os
+from typing import Any
+
+from cohere import AsyncClientV2
+from cohere.types import ChatMessages, ChatResponse
+
+from mcp_goodnews.newsapi import Article
+
+# prompt templates
+DEFAULT_GOODNEWS_SYSTEM_PROMPT = "Given the list of articles, return {num_articles_to_return} of the most positive articles."
+DEFAULT_RANK_INSTRUCTION_TEMPLATE = (
+    "Please rank the articles provided below according to their positivity "
+    "based on their `title` as well as the `content` fields of an article."
+    "\n\n<articles>\n\n{formatted_articles}</articles>"
+)
+
+DEFAULT_NUM_ARTICLES_TO_RETURN = 3
+DEFAULT_MODEL_NAME = "command-r-plus-08-2024"
+
+
+class GoodnewsRanker:
+    def __init__(
+        self,
+        model_name: str = DEFAULT_MODEL_NAME,
+        num_articles_to_return: int = 3,
+        system_prompt_template: str = DEFAULT_GOODNEWS_SYSTEM_PROMPT,
+        rank_instruction_template: str = DEFAULT_RANK_INSTRUCTION_TEMPLATE,
+    ):
+        self.model_name = model_name
+        self.num_articles_to_return = num_articles_to_return
+        self.system_prompt_template = system_prompt_template
+        self.rank_instruction_template = rank_instruction_template
+
+    def _get_client(self) -> AsyncClientV2:
+        """Get cohere async client.
+
+        NOTE: this requires `COHERE_API_KEY` env variable to be set.
+        """
+        return AsyncClientV2(
+            api_key=os.environ.get("COHERE_API_KEY"),
+        )
+
+    def _format_articles(self, articles: list[Article]) -> str:
+        return "\n\n".join(
+            json.dumps(a.model_dump(by_alias=True), indent=4) for a in articles
+        )
+
+    def _prepare_chat_messages(
+        self, articles: list[Article]
+    ) -> list[ChatMessages]:
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt_template.format(
+                    num_articles_to_return=self.num_articles_to_return
+                ),
+            },
+            {
+                "role": "user",
+                "content": self.rank_instruction_template.format(
+                    formatted_articles=self._format_articles(articles)
+                ),
+            },
+        ]
+        return messages
+
+    def _postprocess_chat_response(self, response: ChatResponse) -> str | Any:
+        return response.message.content[0].text
+
+    async def rank_articles(self, articles: list[Article]) -> str:
+        """Uses cohere llms to rank a set of articles."""
+        co = self._get_client()
+        response: ChatResponse = await co.chat(
+            model=self.model_name,
+            messages=self._prepare_chat_messages(articles),
+        )
+        return self._postprocess_chat_response(response)
--- tests/test_newsapi_models/test_schema.py
+++ tests/test_newsapi_models/test_schema.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from mcp_goodnews.newsapi import NewsAPIResponse
+from mcp_goodnews.newsapi import Article, ArticleSource, NewsAPIResponse
 
 
 def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
@@ -8,3 +8,20 @@ def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
 
     assert response.status == "ok"
     assert response.total_results == 10
+    assert all(a.source.id_ == "bbcnews" for a in response.articles)
+
+
+def test_article_source_serialization(
+    example_source_dict: dict[str, Any]
+) -> None:
+    example_source = ArticleSource.model_validate(example_source_dict)
+    serialized = example_source.model_dump(by_alias=True)
+
+    assert serialized == example_source_dict
+
+
+def test_article_serialization(example_article_dict: dict[str, Any]) -> None:
+    example_article = Article.model_validate(example_article_dict)
+    serialized = example_article.model_dump(by_alias=True)
+
+    assert serialized == example_article_dict
