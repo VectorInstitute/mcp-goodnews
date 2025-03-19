e56e81d942e3f6b3a2a8ba981d6c9ba44ded6e68
Val Andrei Fajardo <andrei@nerdai.io>
2025-03-18 23:09:29 -0400

    add unit tests for more serialization of schema obs

--- tests/test_newsapi_models/conftest.py
+++ tests/test_newsapi_models/conftest.py
@@ -14,3 +14,24 @@ def response_json() -> dict[str, Any]:
         response_json = json.load(f)
         response_json = cast(dict[str, Any], response_json)
     return response_json  # type: ignore[no-any-return]
+
+
+@pytest.fixture
+def example_source_dict() -> dict[str, Any]:
+    return {"id": "fake_source_id", "name": "fake_name"}
+
+
+@pytest.fixture
+def example_article_dict(
+    example_source_dict: dict[str, Any]
+) -> dict[str, Any]:
+    return {
+        "source": example_source_dict,
+        "author": "fake author",
+        "title": "fake title",
+        "description": "fake description",
+        "url": "fake url",
+        "urlToImage": "fake url to image",
+        "publishedAt": "fake published at",
+        "content": "fake content",
+    }
--- tests/test_newsapi_models/test_schema.py
+++ tests/test_newsapi_models/test_schema.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from mcp_goodnews.newsapi import ArticleSource, NewsAPIResponse
+from mcp_goodnews.newsapi import Article, ArticleSource, NewsAPIResponse
 
 
 def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
@@ -11,9 +11,17 @@ def test_newsapiresponse_from_json(response_json: dict[str, Any]) -> None:
     assert all(a.source.id_ == "bbcnews" for a in response.articles)
 
 
-def test_article_source_serialization() -> None:
-    source_dict = {"id": "fake_source_id", "name": "fake_name"}
-    source = ArticleSource.model_validate(source_dict)
-    serialized = source.model_dump(by_alias=True)
+def test_article_source_serialization(
+    example_source_dict: dict[str, Any]
+) -> None:
+    example_source = ArticleSource.model_validate(example_source_dict)
+    serialized = example_source.model_dump(by_alias=True)
 
-    assert serialized == source_dict
+    assert serialized == example_source_dict
+
+
+def test_article_serialization(example_article_dict: dict[str, Any]) -> None:
+    example_article = Article.model_validate(example_article_dict)
+    serialized = example_article.model_dump(by_alias=True)
+
+    assert serialized == example_article_dict
