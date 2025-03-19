ac18d0d6d9bd4b75db0cbeeba34a71e9ab37dd8d
Val Andrei Fajardo <andrei@nerdai.io>
2025-03-19 02:26:38 -0400

    server fetch_list_of_goodnews

--- src/mcp_goodnews/server.py
+++ src/mcp_goodnews/server.py
@@ -1,22 +1,35 @@
-# server.py
+"""Goodnews MCP Server"""
+
+import os
+
+import httpx
 from mcp.server.fastmcp import FastMCP
 
+from mcp_goodnews.goodnews_ranker import GoodnewsRanker
+from mcp_goodnews.newsapi import NewsAPIResponse
+
 # Create an MCP server
-mcp = FastMCP("Demo")
+mcp = FastMCP("Goodnews")
 
 
-# Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+async def fetch_list_of_goodnews() -> str:
+    """Fetch a list of headlines and return only top-ranked news based on positivity."""
 
+    # make request to top-headlines newsapi
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            "https://newsapi.org/v2/top-headlines",
+            params={"apiKey": os.environ.get("NEWS_API_KEY")},
+        )
+        response_json = response.json()
+        news_api_response_obj = NewsAPIResponse.model_validate(response_json)
 
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+    # rank the retrieved handlines and get only most positive articles
+    ranker = GoodnewsRanker()
+    goodnews = await ranker.rank_articles(news_api_response_obj.articles)
+
+    return goodnews  # type: ignore[no-any-return]
 
 
 if __name__ == "__main__":
